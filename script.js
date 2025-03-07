92a8bdde8bc69aa363859cad92969eb4fa4ddeba
Jehangir Shah <53529731+jehangir1@users.noreply.github.com>
2025-03-07 23:36:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Adding two numbers at the beginning of an array using unshift() and printing the array.
+let fruitsArray = ["Apple", "Banana", "Orange", "Mango", "Pineapple"];
+let numberArray = [1, 2];
+let addedElement = numberArray.unshift(1, 2);
+alert(numberArray + " " + "are added at the begining of the Array using unshift()." + " " + "Please check the console log."); // 1, 2
+console.log(numberArray);
